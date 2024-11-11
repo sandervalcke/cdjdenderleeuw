7a5905312d39aac6e6482daa417b058ecaf9ee47
Sander Valcke <sander.valcke@gmail.com>
2024-11-11 11:03:33 +0100

    Fix setting the correct site URL
    The astro guide doesn't use ts or not sure what's different, the actual place to set the site is in config.ts (I hope)

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: https://sandervalcke.github.io/cdjdenderleeuw,
+  site: SITE.website,
   integrations: [
     tailwind({
       applyBaseStyles: false,
